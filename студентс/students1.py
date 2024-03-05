2aea42633f601a03a0f8ee1f022ce6306abb6cb1
sonyaaa00 <s.lukyanova0808@mail.ru>
2024-03-05 12:11:28 +0300

    задание 1

--- студентс/students1.py
+++ студентс/students1.py
@@ -6,18 +6,37 @@ class Student:
     fio = ''
     clas = ''
     score = 0
+    title = ''
 
 
 
 for i in range(501):
     students.append(Student())
-    s = f.readline().split(',')
-    if s[4] == None:
-        s[4] = -10
+    s = f.readline().strip().split(',')
     students[i].id = s[0]
     students[i].fio = s[1]
+    students[i].title = s[2]
     students[i].clas = s[3]
     students[i].score = s[4]
+
+sum = 0
+k = 0
+for i in students:
+    if str(i.score) == 'None\n':
+        for j in students:
+            if i.clas == j.clas and j.score != 'None\n':
+                k += 1
+                sum += j.score
+                print(sum)
+        i.score = round(sum/k, 3)
+
+
+
+fil = open('new_students.csv', 'w')
+
+for i in students:
+    print(i.id, ',', i.fio, ',', i.title, ',', i.clas, ',', i.score, file = fil)
+
 
 
 f = 'Хадаров Владимир Валериевич'
